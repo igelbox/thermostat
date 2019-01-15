60d4e61a3cfb1d1a49482987fc422b9ef8947099
Vakhurin Sergey <igelbox@gmail.com>
2019-01-15 03:57:42 +0300

    zero iteration: connected AM2320 temperature sensor, RTC and LCD display

--- main.ino
+++ main.ino
@@ -1,102 +1,37 @@
-#include <DHT.h>
-#include <EEPROM.h>
+#include "Adafruit_AM2320.h"
+#include "RTClib.h"
+#include <LiquidCrystal.h>
 
-DHT dht(2, DHT22);
-unsigned short offset;
-unsigned short eeprom_size;
-
-unsigned short ensure_write(byte size) {
-  unsigned short prev = offset;
-  offset += size;
-  if (offset > eeprom_size) {
-    prev = 2;
-    offset = prev + size;
-  }
-  return prev;
-}
-
-void commit_write() {
-  EEPROM.put(0, offset);
-}
-
-void reset() {
-  Serial.print("EEPROM Erasing ...");
-  for (unsigned i = 0; i < eeprom_size; ++i) {
-    EEPROM.write(i, 0);
-  }
-  Serial.println(" OK");
-  offset = 2;
-}
-
-void printTH(float t, float h) {
-  Serial.print("t:");
-  Serial.print(t);
-  Serial.print(", h:");
-  Serial.println(h);
-}
-
-void dumpCell(unsigned i) {
-  uint8_t t = EEPROM.read(i + 0);
-  uint8_t h = EEPROM.read(i + 1);
-  if (!t && !h) {
-    return;
-  }
-  float tf = ((float)t) / 2.125f - 40.f;
-  float hf = ((float)h) / 2.55f;
-  printTH(tf, hf);
-}
-
-void dump() {
-  for (unsigned i = offset; i < eeprom_size; i += 2) {
-    dumpCell(i);
-  }
-  for (unsigned i = 2; i < offset; i += 2) {
-    dumpCell(i);
-  }
-}
+Adafruit_AM2320 th = Adafruit_AM2320();
+RTC_DS1307 tm;
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 void setup() {
-  Serial.begin(9600);
+  // Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.print("init...");
 
-  dht.begin();
-  eeprom_size = EEPROM.length();
-  EEPROM.get(0, offset);
-  Serial.print("EEPROM Offset: ");
-  Serial.print(offset);
-  Serial.print(", Size: ");
-  Serial.println(eeprom_size);
-  if (offset == 0xFFFF) {
-    reset();
-  } else if (!offset) {
-    offset = 2;
-  }
+  Wire.begin();
+  th.begin();
+  tm.begin();
+
+  lcd.print("ok");
 }
 
+#define out lcd
+
 void loop() {
-  for (int i = 30 * 60; i > 0 ; --i) {
-    if (Serial.available()) {
-      String cmd = Serial.readString();
-      if (cmd == "?") {
-        Serial.println("m, d, c");
-        Serial.print("w:");
-        Serial.println(i / 60.f);
-      } else if (cmd == "m") {
-        float t = dht.readTemperature();
-        float h = dht.readHumidity();
-        printTH(t, h);
-      } else if (cmd == "d") {
-        dump();
-      } else if (cmd == "c") {
-        reset();
-      }
-    }
-    delay(1000);
-  }
-  float t = dht.readTemperature();
-  float h = dht.readHumidity();
-  printTH(t, h);
-  uint8_t i = ensure_write(2);
-  EEPROM.write(i + 0, (uint8_t)((t + 40.f) * 2.125f));
-  EEPROM.write(i + 1, (uint8_t)(h * 2.55f));
-  commit_write();
+  DateTime now = tm.now();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  out.print(th.readTemperature());
+  out.print(" C, ");
+  out.print(th.readHumidity());
+  out.print(" H");
+
+  lcd.setCursor(0, 1);
+  char buff[13];
+  sprintf(buff, "%02d:%02d:%02d.%03d", now.hour(), now.minute(), now.second(), millis() % 1000);
+  out.print(buff);
+  delay(2000);
 }
